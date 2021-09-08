68f214d5bc05774797f2b71a5f6bc1bb563a5d03
Iyadh Faleh <iyadh.faleh@gmail.com>
2021-09-08 14:27:05 +0100

    feat: create jobs migration

---
+++ database/migrations/2021_09_08_131803_jobs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class JobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->uuid('reference');
+            $table->string('name');
+            $table->longText('description');
+            $table->unsignedInteger('schedule_at');
+            $table->unsignedInteger('created_at');
+            $table->index(['name', 'schedule_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
